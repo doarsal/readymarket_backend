7a157d87cf8e62c5bd5c11bbfaffb7c67809e3c4
David Schiro <david.schiro@devbase.us>
2025-10-12 16:26:29 -0300

    #Master - Fix missing import

--- app/Http/Controllers/Api/MitecPaymentController.php
+++ app/Http/Controllers/Api/MitecPaymentController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 /**
  * @OA\Tag(
